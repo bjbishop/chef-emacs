40981f8a62ad8a20000a0b5cd3a2202a1e5e414b
Bryan Bishop <bryan.bishop@pearson.com>
2015-08-22 21:28:19 -0500

    Add emacs package, disable mac keybinding

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,9 @@
-#
-# Cookbook Name:: chef-emacs
-# Recipe:: default
-#
-# Copyright 2015, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+homebrewalt_cask "railwaycat/emacsmacport"
+
+package "emacs-mac"
+
+execute "Turn off Mac keybind 'Control-Command-D'" do
+  action :run
+  command "defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 70 '<dict><key>enabled</key><false/></dict>'"
+  user node['current_user']
+end
