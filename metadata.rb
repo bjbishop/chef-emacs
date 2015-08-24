71f0719fdbdea6312bf92166874dda59bc035a47
Bryan Bishop <bryan.bishop@pearson.com>
2015-08-23 21:48:14 -0500

    Restore .emacs.d, change cookbook description

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,9 @@ name             'chef-emacs'
 maintainer       'Bryan Bishop'
 maintainer_email 'bryanjbishop@yahoo.com'
 license          'All rights reserved'
-description      'Installs/Configures chef-emacs'
+description      'Installs and restores emacs to my preferences'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "homebrewalt"
+depends "duplicity"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,5 @@
+include_recipe "chef-duplicity::default"
+
 # railway's isn't working right now
 
 # homebrewalt_tap "railwaycat/emacsmacport"
@@ -13,3 +15,21 @@ execute "Turn off Mac keybind 'Control-Command-D'" do
 end
 
 Chef::Log.info "#{cookbook_name}: A reboot is needed for the Mac keybind to take effect."
+
+passwd = ::File.readlines(::File.join(::Dir.home(node['current_user']), '.box'))[1].chomp
+
+duplicity_restore "Restore folder .emacs.d into ~/.emacs.d" do
+  restore_item ".emacs.d"
+  local_path File.join(::Dir.home(node["current_user"]), ".emacs.d")
+  remote_path node["duplicity"]["webdav_remote"]
+  restore_as_user node['current_user']
+  restore_as_group node['current_user']
+  remote_path_password passwd
+  encryption_password passwd
+  # If restoring files from a backup using a different UID,
+  # use the option: --ignore-errors
+  duplicity_options [
+    "--no-print-statistics",
+    "--numeric-owner",
+  ]
+end
